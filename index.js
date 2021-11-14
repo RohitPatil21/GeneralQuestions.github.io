106dde9a616be2b890d6ce89f1ef8e38652ebca4
Rohit <rohitpatil7@yahoo.com>
2021-11-14 16:20:58 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// question 1 answer 
+function que1(){
+    let plusIconRef = document.getElementById('m-i1');
+    let answerBoxRef = document.getElementById('ans1');
+    answerBoxRef.style.display = "block"
+    answerBoxRef.innerHTML = "Answer : Yes"
+}
+
+
+// Question 2 answer 
+function que2(){
+    let plusIconRef = document.getElementById('m-i2');
+    let answerBoxRef = document.getElementById('ans2');
+    answerBoxRef.style.display = "block"
+    answerBoxRef.innerHTML = "Answer : Yes"
+}
+
+// Question 3 answer 
+function que3(){
+    let plusIconRef = document.getElementById('m-i3');
+    let answerBoxRef = document.getElementById('ans3');
+    answerBoxRef.style.display = "block"
+    answerBoxRef.innerHTML = "Answer : Yes"
+}
+
+
+// Question 4 answer 
+function que4(){
+    let plusIconRef = document.getElementById('m-i4');
+    let answerBoxRef = document.getElementById('ans4');
+    answerBoxRef.style.display = "block"
+    answerBoxRef.innerHTML = "Answer : No"
+}
+
+
+// Question 5 answer 
+function que5(){
+    let plusIconRef = document.getElementById('m-i5');
+    let answerBoxRef = document.getElementById('ans5');
+    answerBoxRef.style.display = "block"
+    answerBoxRef.innerHTML = "Answer : Shri Arvind Kejriwal"
+}
